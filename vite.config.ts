9f47ebd2ef858fd48e426329bf774de91237c606
Jasper Pell <72871978+Baggins5066@users.noreply.github.com>
2025-11-02 19:40:04 +0000

    Added replit app to allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
         port: 5000,
         host: '0.0.0.0',
         strictPort: true,
-        allowedHosts: ['.replit.dev'],
+        allowedHosts: ['.replit.dev', 'bagginsite.replit.app'],
       },
       plugins: [react()],
       define: {

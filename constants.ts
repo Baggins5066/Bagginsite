6aa9ffd350b7fb5acb3f0d210ce42c3056ac8421
Jasper Pell <72871978+Baggins5066@users.noreply.github.com>
2025-11-02 13:30:34 -0500

    Import images so Vite can process them

--- constants.ts
+++ constants.ts
@@ -1,10 +1,12 @@
 import { Bot } from './types';
+import meatBroAvatar from './assets/Meat-Bro.png';
+import blevitronAvatar from './assets/Blevitron.png';
 
 export const BOTS_DATA: Bot[] = [
   {
     id: '1415012919921610864',
     name: 'Meat Bro',
-    avatarUrl: 'assets/Meat-Bro.png',
+    avatarUrl: meatBroAvatar,
     description: "A brash and confident member of the Raw Meat Boyz movement; with a casual, slang-filled persona, he engages users while promoting confidence and camaraderie. Whether you're after motivation or just some banter, Meat Bro is here to enhance your Discord vibe!",
     tags: [{ name: 'AI', icon: 'cognition_2' }, { name: 'Character', icon: 'person_book' }],
     administratorInviteUrl: 'https://discord.com/oauth2/authorize?client_id=1415012919921610864&permissions=8&integration_type=0&scope=bot',
@@ -15,7 +17,7 @@ export const BOTS_DATA: Bot[] = [
   {
     id: '1430009844680884405',
     name: 'Blevitron',
-    avatarUrl: 'assets/Blevitron.png',
+    avatarUrl: blevitronAvatar,
     description: 'Blevitron is a synthesized persona based on a real human personality. She is rude, humorous, and cares little for others. She might even treat you differently if she knows who you are! Blevitron is a sure way to make your Discord server more interesting.',
     tags: [{ name: 'AI', icon: 'cognition_2' }, { name: 'Commands', icon: 'data_object' }, { name: 'Humanoid', icon: 'person' }],
     administratorInviteUrl: 'https://discord.com/oauth2/authorize?client_id=1430009844680884405&permissions=8&integration_type=0&scope=bot',
